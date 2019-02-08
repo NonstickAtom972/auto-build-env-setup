e0f11f70a71853fea49fd6944383fd685f3c9ca6
AustinGarrick1818 <47135742+AustinGarrick1818@users.noreply.github.com>
2019-02-07 19:20:41 -0800

    Add files via upload

---
+++ auto-build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Bare bines build script
+
+# Source setup env
+. build/envsetup.sh
+
+# Make CV1 (LG Phoenix 4 / LG Aristo 2) chosen for build
+linch lineage_cv1-userdebug
+
+# Before build
+croot
+
+# Start the build
+mka -j8 bacon # Use with amount of cores you have
