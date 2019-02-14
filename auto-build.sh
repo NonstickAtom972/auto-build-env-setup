fb23e8aa9bda0d47c9bb8d066ad01f456794aee1
NonstickAtom972 <47039222+NonstickAtom972@users.noreply.github.com>
2019-02-14 10:40:33 -0600

    added more repo installers

--- auto-build.sh
+++ auto-build.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 # Bare bones build script
 
+# Update Existing Repo's
+sudo apt-get update
+
+# Install git
+sudo apt-get install git
+
+# Install git gui
+sudo apt-get install gitk git-gui
+
+
 # Source setup env
 . build/envsetup.sh
 
-# Make CV1 (LG Phoenix 4 / LG Aristo 2) chosen for build
+# Make cv1 (LG Phoenix 4 / LG Aristo 2) chosen for build
 lunch lineage_cv1-userdebug
 
 # Before build
@@ -12,3 +22,4 @@ croot
 
 # Start the build
 mka -j8 bacon # Use with amount of cores you have
+
