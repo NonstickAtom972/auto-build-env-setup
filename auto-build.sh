8a65c01be7950c30035158a0cd1803c98e9d3323
AustinGarrick1818 <47135742+AustinGarrick1818@users.noreply.github.com>
2020-06-14 14:06:31 -0700

    Update auto-build.sh

--- auto-build.sh
+++ auto-build.sh
@@ -2,11 +2,12 @@
 
 # Android x86 (PC) build script
 
-# Source env file(s)
+SCRIPT_VER="v1.0.1"
+LOG_TMP="/tmp/build.log"
+
+
 . build/envsetup.sh
 
-# Run our lunch command to select build
 lunch android_x86-eng
 
-# Build the image
 m -j1 iso_img
